114d4ebc59d39a83794ed04ece90ffa6e09139d5
Mariia Khapun <111394270+AlluringUnicorn@users.noreply.github.com>
2023-08-01 19:26:13 +0300

    added/prepopulated notes, fixed/date, format,

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
-// Import the necessary DOM elements
 const notesTableBody = document.getElementById("notes-table-body");
-const archivedNotesTableBody = document.getElementById("archived-notes-table-body");
+const archivedNotesTableBody = document.getElementById(
+  "archived-notes-table-body"
+);
 const summaryTableBody = document.getElementById("summary-table-body");
 
-// Example data for the notes app (7 prepopulated notes)
 let notes = [
   {
     id: 1,
-    createdAt: new Date("2023-07-20"),
-    content: "I'm going to the dentist tomorrow",
+    createdAt: new Date("2023-07-19"),
+    content: "Dentist 20/07/2023 and then 25/07/2023",
     category: "Task",
   },
-  // Add other prepopulated notes here...
+  {
+    id: 2,
+    createdAt: new Date("2023-05-10"),
+    content: "Start coffee business",
+    category: "Idea",
+  },
+  {
+    id: 3,
+    createdAt: new Date("2023-07-12"),
+    content: "Buy earrings from Instagram shop",
+    category: "Random Thought",
+  },
+  {
+    id: 4,
+    createdAt: new Date("2023-06-14"),
+    content: "Birthday present for Katya - Pandora ring",
+    category: "Idea",
+  },
+  {
+    id: 5,
+    createdAt: new Date("2023-05-18"),
+    content: "Send documents to partner 05/08/2023",
+    category: "Task",
+  },
+  {
+    id: 6,
+    createdAt: new Date("2023-07-29"),
+    content: "Budynok dyva in Kyiv",
+    category: "Random Thought",
+  },
+  {
+    id: 7,
+    createdAt: new Date("2023-07-24"),
+    content: "Date idea: Museum of water visit",
+    category: "Idea",
+  },
 ];
 
-// Function to render the notes table
+
 function renderNotesTable() {
   notesTableBody.innerHTML = "";
   notes.forEach((note) => {
     const row = document.createElement("tr");
     row.innerHTML = `
-      <td>${note.createdAt.toLocaleString()}</td>
-      <td>${note.content}</td>
+      <td>${formatDate(note.createdAt)}</td>
       <td>${note.category}</td>
+      <td>${note.content}</td>
       <td>${extractDatesFromString(note.content).join(", ")}</td>
       <td>
         <button onclick="editNote(${note.id})">Edit</button>
@@ -33,16 +68,16 @@ function renderNotesTable() {
   });
 }
 
-// Function to render the archived notes table
+
 function renderArchivedNotesTable() {
   archivedNotesTableBody.innerHTML = "";
   const archivedNotes = notes.filter((note) => note.archived);
   archivedNotes.forEach((note) => {
     const row = document.createElement("tr");
     row.innerHTML = `
-      <td>${note.createdAt.toLocaleString()}</td>
-      <td>${note.content}</td>
+      <td>${formatDate(note.createdAt)}</td>
       <td>${note.category}</td>
+      <td>${note.content}</td>
       <td>${extractDatesFromString(note.content).join(", ")}</td>
       <td>
         <button onclick="unarchiveNote(${note.id})">Unarchive</button>
@@ -52,19 +87,22 @@ function renderArchivedNotesTable() {
   });
 }
 
-// Function to extract dates from the note content using regular expressions
+
 function extractDatesFromString(content) {
   const datePattern = /\d{1,2}\/\d{1,2}\/\d{4}/g;
   return content.match(datePattern) || [];
 }
 
-// Function to update the summary table
 function updateSummaryTable() {
   const categories = ["Task", "Random Thought", "Idea"];
   summaryTableBody.innerHTML = "";
   categories.forEach((category) => {
-    const activeCount = notes.filter((note) => !note.archived && note.category === category).length;
-    const archivedCount = notes.filter((note) => note.archived && note.category === category).length;
+    const activeCount = notes.filter(
+      (note) => !note.archived && note.category === category
+    ).length;
+    const archivedCount = notes.filter(
+      (note) => note.archived && note.category === category
+    ).length;
     const row = document.createElement("tr");
     row.innerHTML = `
       <td>${category}</td>
@@ -76,7 +114,6 @@ function updateSummaryTable() {
 }
 
 
-// Function to edit a note
 function editNote(noteId) {
   const noteIndex = notes.findIndex((note) => note.id === noteId);
   if (noteIndex !== -1) {
@@ -89,7 +126,7 @@ function editNote(noteId) {
   }
 }
 
-// Function to archive a note
+
 function archiveNote(noteId) {
   const noteIndex = notes.findIndex((note) => note.id === noteId);
   if (noteIndex !== -1) {
@@ -100,7 +137,7 @@ function archiveNote(noteId) {
   }
 }
 
-// Function to unarchive a note
+
 function unarchiveNote(noteId) {
   const noteIndex = notes.findIndex((note) => note.id === noteId);
   if (noteIndex !== -1) {
@@ -110,8 +147,17 @@ function unarchiveNote(noteId) {
   }
 }
 
+function formatDate(date) {
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    timeZone: "UTC",
+  };
+  return new Intl.DateTimeFormat("en-US", options).format(date);
+}
 
-// Initial rendering of the app
+
 renderNotesTable();
 renderArchivedNotesTable();
 updateSummaryTable();
